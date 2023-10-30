0c7378623a506d49fae9a962a264e0764a4cd20b
Ricky <kaustav.ray_bca21@gla.ac.in>
2023-10-30 22:37:30 +0530

    modified register component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './register/register.component';
-
-
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
-  declarations: [
-    RegisterComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    RegisterComponent
-  ]
+	declarations: [RegisterComponent],
+	imports: [CommonModule, AppRoutingModule],
+	exports: [RegisterComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
