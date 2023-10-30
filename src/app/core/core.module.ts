644753edc5e54d86beedfef29c36496c7e899379
Ricky <kaustav.ray_bca21@gla.ac.in>
2023-10-30 20:10:23 +0530

    added header to app

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header.component';
-
-
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    HeaderComponent
-  ]
+	declarations: [HeaderComponent],
+	imports: [CommonModule],
+	exports: [HeaderComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
