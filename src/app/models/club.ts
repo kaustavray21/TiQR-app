4fc175b0a4c3cf5a7f054773a3d3521a3951438e
Ricky <kaustav.ray_bca21@gla.ac.in>
2023-11-21 14:01:13 +0530

    created dummy clubs

---
+++ src/app/models/club.ts
@@ -0,0 +1,39 @@
+export class clubs {
+	id: number;
+	name: string;
+	motto: string;
+	mentor: string;
+	email: string;
+	president: string;
+	vicePresident: string;
+	generalSecretary: string;
+	mainEvent: string;
+	strength: number;
+	level: string;
+
+	constructor(
+		id: number,
+		name: string,
+		motto: string,
+		mentor: string,
+		email: string,
+		president: string,
+		vicePresident: string,
+		generalSecretary: string,
+		mainEvent: string,
+		strength: number,
+		level: string
+	) {
+		this.id = id;
+		this.name = name;
+		this.motto = motto;
+		this.mentor = mentor;
+		this.email = email;
+		this.president = president;
+		this.vicePresident = vicePresident;
+		this.generalSecretary = generalSecretary;
+		this.mainEvent = mainEvent;
+		this.strength = strength;
+		this.level = level;
+	}
+}
--- src/app/shared/clubs/clubs.component.ts
+++ src/app/shared/clubs/clubs.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
 	styleUrls: ['./clubs.component.scss'],
 })
 export class ClubsComponent {
-	clubs: Number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	clubs: Number[] = [];
 }
